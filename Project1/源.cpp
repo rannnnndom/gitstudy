f82aa7b6bc1577e438f9f907468837198689a29f
KLWXD <2856594865@qq.com>
2023-03-14 14:49:12 +0800

    涛哥速速与我击剑

---
+++ Project1/源.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "hello world!";
+	cout << "******************"<<endl;
+	cout << "*涛哥速速与我击剑*" << endl;
+	cout << "*涛哥速速与我击剑*" << endl;
+	cout << "*涛哥速速与我击剑*" << endl;
+	cout << "*涛哥速速与我击剑*" << endl;
+	cout << "*涛哥速速与我击剑*" << endl;
+	cout << "*涛哥速速与我击剑*" << endl;
+	cout << "*涛哥速速与我击剑*" << endl;
+	cout << "******************" << endl;
+
+	return 0;
+}
